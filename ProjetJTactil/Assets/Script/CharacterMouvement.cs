b799ec00f9da6ea75f48e1c4fdafaf769c3bcde6
HajnalFlorian <32631861+HajnalFlorian@users.noreply.github.com>
2021-03-26 18:26:27 +0100

    Finsh

--- ProjetJTactil/Assets/Script/CharacterMouvement.cs
+++ ProjetJTactil/Assets/Script/CharacterMouvement.cs
@@ -12,7 +12,16 @@ public class CharacterMouvement : MonoBehaviour
 
     Rigidbody rb;
 
+<<<<<<< Updated upstream
     public float speed = 10f;
+=======
+    public GameObject score;
+    public ScoreManager scoreManager;
+    public Text scoreText;
+    public Text highScoreText;
+
+    private float speed = 8.0f;
+>>>>>>> Stashed changes
     public float gyroSpeed = 5f;
     public float jumpForce = 0.1f;
 
@@ -21,7 +30,14 @@ public class CharacterMouvement : MonoBehaviour
     private float tempsMax = 0.1f;
     private bool allowCpt;
 
+<<<<<<< Updated upstream
     bool Alive = true;
+=======
+    private float scoreF;
+
+
+    public bool Alive;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +49,47 @@ public class CharacterMouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         dirX = Input.acceleration.x * gyroSpeed;
+=======
+        if (Alive)
+        {
+            dirX = Input.acceleration.x * gyroSpeed;
+
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        startPos = touch.position;
+                        allowCpt = true;
+                        break;
+
+                    case TouchPhase.Moved:
+                        direction = touch.position - startPos;
+                        break;
+
+                    case TouchPhase.Ended:
+                        //JumpIfAllowed();
+                        break;
+                }
+
+            }
+
+            if (allowCpt)
+            {
+                cpt += Time.deltaTime;
+
+                if (cpt >= tempsMax)
+                {
+                    allowCpt = false;
+                    JumpIfAllowed();
+                    cpt = 0;
+                }
+            }
+>>>>>>> Stashed changes
 
         if (Input.touchCount > 0)
         {
@@ -73,7 +129,11 @@ public class CharacterMouvement : MonoBehaviour
         {
             return;
         }
+<<<<<<< Updated upstream
         if (transform.position.y < -5)
+=======
+        else if (!Alive)
+>>>>>>> Stashed changes
         {
             Die();
         }
@@ -91,9 +151,25 @@ public class CharacterMouvement : MonoBehaviour
 
     public void Die()
     {
+<<<<<<< Updated upstream
         Alive = false;
         // restart the game 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+=======
+        deatthMenu.SetActive(true);
+        Time.timeScale = 0;
+        score.SetActive(false);
+        scoreF = scoreManager.score;
+        if(PlayerPrefs.GetFloat("HighScore") < scoreF)
+        {
+            PlayerPrefs.SetFloat("HighScore", scoreF);
+        }
+        highScoreText.text = "highScore: " + Mathf.Round(PlayerPrefs.GetFloat("HighScore"));
+        Debug.Log(PlayerPrefs.GetFloat("HighScore"));
+        Debug.Log(scoreF);
+
+        scoreText.text = "Score: " + Mathf.Round(scoreManager.score);
+>>>>>>> Stashed changes
     }
     
 
@@ -122,4 +198,18 @@ public class CharacterMouvement : MonoBehaviour
 
         
     }
+<<<<<<< Updated upstream
+=======
+    // augmentation de la vitesse
+    public void setSpeed(float modifier)
+    {
+        speed = 8.0f + modifier;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+       // SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+    }
+>>>>>>> Stashed changes
 }
---
+++ ProjetJTactil/Assets/Script/Sceneswap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneswap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void quitApp()
+    {
+        Application.Quit();
+    }
+
+}
